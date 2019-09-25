e6f22a7f9ba884b05be23902ec23d67cac5f3a77
migmeneses <mig_jobs@yahoo.com>
2019-09-25 16:21:16 +0000

    Added -y on commands

--- 01_install_docker.sh
+++ 01_install_docker.sh
@@ -4,7 +4,7 @@
 # First, update your existing list of packages:
 sudo apt update ; sleep 2s
 # Next, install a few prerequisite packages which let apt use packages over HTTPS:
-sudo apt install apt-transport-https ca-certificates curl software-properties-common ; sleep 2s
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y ; sleep 2s
 # Then add the GPG key for the official Docker repository to your system:
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - ; sleep 2s
 # Add the Docker repository to APT sources:
@@ -12,7 +12,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 # Next, update the package database with the Docker packages from the newly added repo:
 sudo apt update ; sleep 2s
 # Finally, install Docker:
-sudo apt install docker-ce ;  sleep 2s
+sudo apt install docker-ce -y ;  sleep 2s
 # Docker should now be installed, the daemon started, and the process enabled to start on boot. Check that it’s running:
 sudo systemctl status docker ; sleep 2s
 # Enable docker to start on boot
